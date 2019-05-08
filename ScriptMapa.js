ea12cdc1126f8622425b7e1b51f38eabbbeba609
brayanpasa99 <brayanpasa99@hotmail.com>
2019-05-07 20:12:13 -0500

    Mapa

---
+++ ScriptMapa.js
@@ -0,0 +1,13 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+        source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([-74.488, 4]),
+        zoom: 5
+    })
+});
+
